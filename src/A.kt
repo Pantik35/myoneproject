d714f119ffe075b81aaf95dd9c09e7bab9c2973a
Pantik35 <pantelejcerkun382@gmail.com>
2022-03-20 17:53:01 +0300

    init

--- src/A.kt
+++ src/A.kt
@@ -1,17 +1,38 @@
-fun main(args: Array<String>) {
-    print("Имя посетителя:")
-    val name = readLine()
-    print("Фамилия посетителя:")
-    val seName = readLine()
-    print("Возраст посетителя:")
-    val age = readLine().toString()
-    val a = age.toInt()
-    if(a >= 18)
-        println("Поситителя $name $seName пропустить в торговый центр")
-    else
-        if (a< 14)
-            println("Вызвать полицию для поситителя $name $seName")
-        else
-            if (a >= 14)
-                println("Поситителя $name $seName не пропускать в тороговый центр")
+fun main(args:Array<String>) {
+    val arraySize = 5
+    var countPeople = 0
+    val nameArray = arrayOfNulls<String>(arraySize)
+    val surnameArray = arrayOfNulls<String>(arraySize)
+    val ageArray = arrayOfNulls<Int>(arraySize)
+
+
+    while (countPeople < 5) {
+        println("age=")
+        val age = readLine().toString()
+        val a = age.toInt()
+        println("name=")
+        val name = readLine()
+        println("surname=")
+        val surname = readLine()
+
+
+        if (a >= 18) {
+            nameArray[countPeople] = name
+            ageArray[countPeople] = a
+            surnameArray[countPeople] = surname
+            countPeople++
+
+
+        }
+        else {
+
+        }
+
+    }
+    println("name=${nameArray[0]} surname=${surnameArray[0]} age=${ageArray[0]}")
+    println("name=${nameArray[1]} surname=${surnameArray[1]} age=${ageArray[1]}")
+    println("name=${nameArray[2]} surname=${surnameArray[2]} age=${ageArray[2]}")
+    println("name=${nameArray[3]} surname=${surnameArray[3]} age=${ageArray[3]}")
+    println("name=${nameArray[4]} surname=${surnameArray[4]} age=${ageArray[4]}")
+
 }
